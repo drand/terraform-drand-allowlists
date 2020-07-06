64dee7cf76a7ff6eb2c284e1b30de31c826e2df4
Hector Sanjuan <code@hector.link>
2020-07-06 16:03:58 +0200

    Add support for ipv6 cidrs

--- main.tf
+++ main.tf
@@ -4,3 +4,10 @@ data "http" "allowlist_csv" {
     Authorization = "Basic ${base64encode(join(":", [var.username, var.password]))}"
   }
 }
+
+data "http" "allowlist_ip6_csv" {
+  url = "https://allowlists.drand.sh/${var.env}/allowlist_ip6.csv"
+  request_headers = {
+    Authorization = "Basic ${base64encode(join(":", [var.username, var.password]))}"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
 output "cidrs" {
-  description = "A list of CIDRs as specified in the allowlist"
+  description = "A list of IPv4 CIDRs as specified in the allowlist"
   value       = split(",", chomp(data.http.allowlist_csv.body))
+}
+
+output "cidrs6" {
+  description = "A list of IPv6 CIDRs as specified in the allowlist"
+  value       = split(",", chomp(data.http.allowlist_ip6_csv.body))
 }
