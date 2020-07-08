b29061136cd41e333569cb9cbf7b2597ed53f6bc
Hector Sanjuan <code@hector.link>
2020-07-08 16:58:34 +0200

    Use decodejson instead of csv parsing (works with empty lists)

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 data "http" "allowlist_csv" {
-  url = "https://allowlists.drand.sh/${var.env}/allowlist.csv"
+  url = "https://allowlists.drand.sh/${var.env}/allowlist.json"
   request_headers = {
     Authorization = "Basic ${base64encode(join(":", [var.username, var.password]))}"
   }
 }
 
 data "http" "allowlist_ip6_csv" {
-  url = "https://allowlists.drand.sh/${var.env}/allowlist_ip6.csv"
+  url = "https://allowlists.drand.sh/${var.env}/allowlist_ip6.json"
   request_headers = {
     Authorization = "Basic ${base64encode(join(":", [var.username, var.password]))}"
   }
